53be04ade39cbe2c762077ee46da5c95f3167702
Yehonathan Sharvit <viebel@gmail.com>
2019-08-29 22:53:36 +0300

    fix typo in the name of the repo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject viebel/server.http.chester "0.1.0"
   :description "Integrant methods for running a Chester web server"
-  :url "https://github.com/viebel/server.htttp.chester"
+  :url "https://github.com/viebel/server.http.chester"
   :license {:name "EPL-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[com.cycognito/chester "0.2.0"]
