69c8b0f7507e5c6e0c78a34fbccfedd20bd9c09d
Yehonathan Sharvit <viebel@gmail.com>
2021-02-24 15:11:13 +0200

    add scope provided to all deps

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject viebel/server.http.chester "0.1.0"
+(defproject viebel/server.http.chester "0.1.2"
   :description "Integrant methods for running a Chester web server"
   :url "https://github.com/viebel/server.http.chester"
   :license {:name "EPL-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[com.cycognito/chester "0.2.0"]
-                 [duct/core "0.7.0"]
-                 [duct/logger "0.3.0"]
-                 [integrant "0.7.0"]
-                 [org.clojure/clojure "1.10.0"]]
-  :repositories [["snapshots" {:url "https://maven.cyco.fun/snapshots"
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[com.cycognito/chester "0.2.0" :scope "provided"]
+                 [duct/core "0.7.0" :scope "provided"]
+                 [duct/logger "0.3.0" :scope "provided"]
+                 [integrant "0.7.0" :scope "provided"]
+                 [org.clojure/clojure "1.10.0" :scope "provided"]]
+  :repositories [["snapshots" {:url      "https://maven.cyco.fun/snapshots"
                                :username :env/cyco_maven_username
                                :password :env/cyco_maven_password}]
-                 ["releases" {:url "https://maven.cyco.fun/releases"
+                 ["releases" {:url      "https://maven.cyco.fun/releases"
                               :username :env/cyco_maven_username
-                              :password _:env/cyco_maven_password}]])
+                              :password :env/cyco_maven_password}]])
